09297582969406346b8367f02fa40f82d3461d8b
coYjia <455717253@qq.com>
2017-11-23 17:14:11 +0800

    2017.11.23

---
+++ node/2017.11.23/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+http.createServer(function(request,response){
+	var path = url.parse(request.url).pathname;
+	console.log(path);
+	fs.readFile(path.substr(1),function(err,data){
+		if(err){
+			response.writeHead(404,{'Content-Type':'text/html'})
+		}else{
+			response.writeHead(200,{'Content-Type':'text/html'})
+			response.write(data.toString());
+		}
+		response.end();
+	})
+}).listen(8081);
